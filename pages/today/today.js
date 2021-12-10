f5910c67c6fe86f87768516570ec1b565088447f
krishnaamen <krishnaament@gmail.com>
2021-12-10 13:12:23 +0100

    updates html

--- pages/today/today.js
+++ pages/today/today.js
@@ -1,5 +1,14 @@
 import utils from "./../../utils.js";
 
 export default (auth) => {
-	console.log("hello");
+	fetch("http://3.90.205.148/schedules/today")
+		.then((response) => response.json())
+		.then((movieDetails) => {
+			console.log(scheduleDetails);
+            const movieDetailElement = document.querySelector(".movie-details");
+            scheduleDetails.forEach(movietoshow => {
+                
+                
+            });
+		});
 };
