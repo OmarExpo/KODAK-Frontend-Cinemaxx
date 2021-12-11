e811c4e8c918006129a53ec8df7968a7014be891
shpomp <karolina.urnieziute@gmail.com>
2021-12-11 16:42:42 +0100

    small cleanup

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,6 +1,5 @@
 export default () => {
   const displayDiv = document.querySelector(".timeslots");
-  //const mainContentCard = document.querySelector("#main-content-card");
   const timeslotsCard = document.querySelector("#timeslots-card");
   const hallsCard = document.querySelector("#halls-card");
   const movieDisplayCard = document.querySelector("#movie-display-card");
@@ -65,12 +64,11 @@ export default () => {
     });
   })();
 
-  // slot buttons
+  // render slot buttons
   (function renderSlotButtons() {
     slotButtonArray = Array.from(
       document.getElementsByClassName("timeslot-button")
     );
-
     slotButtonArray.forEach((slot) => {
       slot.addEventListener("click", (event) => {
         selectedSlot = slot.innerText;
@@ -82,56 +80,40 @@ export default () => {
     });
   })();
 
-  // hall buttons
-  hallButtonArray = Array.from(document.getElementsByClassName("hall-button"));
-
-  hallButtonArray.forEach((hall) => {
-    hall.addEventListener("click", (event) => {
-      mapBackgroundColor(hallButtonArray, "white");
-      event.target.style.backgroundColor = "blue";
-      selectedHall = hall.id;
+  // render hall buttons
+  (function renderHallButtons() {
+    hallButtonArray = Array.from(
+      document.getElementsByClassName("hall-button")
+    );
+    hallButtonArray.forEach((hall) => {
+      hall.addEventListener("click", (event) => {
+        mapBackgroundColor(hallButtonArray, "white");
+        event.target.style.backgroundColor = "blue";
+        selectedHall = hall.id;
+      });
     });
-  });
+  })();
 
-  // show movie button
+  // define show movie button
   let showMovieButton = document.getElementById("show-button");
   showMovieButton.addEventListener("click", function () {
     movieDisplayCard.style.display = "block";
     fetchAndRenderMovie(selectedDate, selectedSlot, selectedHall);
   });
 
+  // --------------------- fetch and render movie ---------------------
   function fetchAndRenderMovie(date, slot, hall) {
     movieDiv.innerHTML = "";
     const scheduleAPIurl = `http://3.90.205.148/schedules/${date}/${slot}/${hall}`;
     fetch(scheduleAPIurl)
       .then((response) => response.json())
       .then((scheduleData) => {
-        console.log(scheduleData);
         movieid = scheduleData[0].movieId ? scheduleData[0].movieId : movieid;
         const singleMovieAPIurl = `http://3.90.205.148/movies/${movieid}`;
         fetch(singleMovieAPIurl)
           .then((response) => response.json())
           .then((movieData) => {
-            console.log(movieData[0]);
-            moviename = movieData[0].title;
-            const movieTitleobj = document.createElement("h1");
-            movieTitleobj.innerHTML = movieData[0].title;
-            const buttonFreeSeats = document.createElement("button");
-            buttonFreeSeats.innerText = "show free seats";
-            const descriptionObject = document.createElement("P");
-            descriptionObject.innerHTML = movieData[0].story;
-            movieDiv.appendChild(movieTitleobj);
-            get_image_url_by_movie_name(moviename, movieDiv);
-            movieDiv.appendChild(descriptionObject);
-            let freeSeatsButton = document.createElement("button");
-            freeSeatsButton.innerHTML = "see free seats";
-            movieDiv.appendChild(freeSeatsButton);
-
-            freeSeatsButton.addEventListener("click", () => {
-              availSeatsCard.style.display = "block";
-              const apiUrl3 = `http://3.90.205.148/bookings/freeseats/${date}/${hall}/${slot}`;
-              createSeatsArrayFromApi(16, apiUrl3);
-            });
+            renderMovie(movieData[0]);
           })
           .catch((error) => {
             console.log(error);
@@ -139,127 +121,38 @@ export default () => {
       });
   }
 
-  ///-------------------------------------------------------------------
-  let freeseats_button_array = [];
+  function renderMovie(movieData) {
+    console.log(movieData);
+    moviename = movieData.title;
+    const movieTitleobj = document.createElement("h1");
+    movieTitleobj.innerHTML = movieData.title;
+    const buttonFreeSeats = document.createElement("button");
+    buttonFreeSeats.innerText = "show free seats";
+    const descriptionObject = document.createElement("P");
+    descriptionObject.innerHTML = movieData.story;
+    movieDiv.appendChild(movieTitleobj);
+    renderMoviePoster(moviename, movieDiv);
+    movieDiv.appendChild(descriptionObject);
+    let freeSeatsButton = document.createElement("button");
+    freeSeatsButton.innerHTML = "see free seats";
+    movieDiv.appendChild(freeSeatsButton);
 
-  function createSeatsArrayFromApi(totalSeats, apistring) {
-    let freeseats = [];
-    for (let i = 1; i <= totalSeats; i++) {
-      freeseats.push(i);
-    }
-    const trElement = document.querySelector(".tr");
-    let counter = 0;
-    fetch(apistring)
-      .then((response) => response.json())
-      .then((seatsData) => {
-        seatsData.forEach((seat) => {
-          for (let i = 0; i < freeseats.length; i++) {
-            if (freeseats[i] === seat.seatNumber) {
-              freeseats.splice(i, 1);
-            }
-          }
-
-          if (counter % 4 === 0) {
-            const newTd = document.createElement("td");
-            const newTr = document.createElement("tr");
-            const buttonObj = document.createElement("button");
-            buttonObj.innerText = seat.seatNumber;
-            freeseats_button_array.push(buttonObj);
-            newTd.style.backgroundColor = "green";
-            newTd.appendChild(buttonObj);
-            trElement.appendChild(newTr);
-            trElement.appendChild(newTd);
-          } else {
-            const newTd = document.createElement("td");
-            const buttonObj = document.createElement("button");
-            buttonObj.innerText = seat.seatNumber;
-            freeseats_button_array.push(buttonObj);
-            newTd.style.backgroundColor = "green";
-            newTd.appendChild(buttonObj);
-            trElement.appendChild(newTd);
-          }
-
-          counter++;
-        });
-        for (let i = 0; i < freeseats.length; i++) {
-          const newTd = document.createElement("td");
-          const buttonObj = document.createElement("button");
-          buttonObj.innerText = freeseats[i];
-          buttonObj.disabled = "true";
-          newTd.style.backgroundColor = "red";
-          newTd.appendChild(buttonObj);
-          trElement.appendChild(newTd);
-        }
-        freeseats_button_array.forEach((btn) => {
-          btn.addEventListener("click", () => {
-            selected_seat = btn.innerText;
-
-            if (typeof localStorage.username !== "undefined") {
-              const connformation = confirm(
-                "Do you want to reserve seat number:- " + btn.innerText + "?"
-              );
-              if (connformation) {
-                const api4 = `http://3.90.205.148/bookings/${selectedDate}/${moviename}/${selectedHall}/${selectedSlot}/${user_id}/${btn.innerText}`;
-                fetch(api4)
-                  .then((response) => response.json())
-                  .then((reserved_seat) => {
-                    const message_displayObj = document.querySelector(
-                      ".conformation_message"
-                    );
-                    const messageHeader = document.createElement("h3");
-                    let objectLength = Object.keys(reserved_seat).length;
-                    if (objectLength > 0) {
-                      messageHeader.innerHTML = `You have reserved a seat number ${selected_seat},\n
-																		in hall no ${selectedHall} on ${selectedDate},${selectedSlot} show on\n
-																		to see the ${moviename} . Thank you for choosing us... have a nice show.`;
-
-                      message_displayObj.appendChild(messageHeader);
-                      btn.style.backgroundColor = "blue";
-                      btn.disabled = "true";
-                      trElement.style.display = "none";
-                      headerTag_element.innerHTML = "Your reservation datails.";
-                      const print_btn = document.createElement("button");
-                      print_btn.className = "print_btn";
-                      print_btn.innerText = "Print";
-                      message_displayObj.appendChild(print_btn);
-                      print_btn.addEventListener("click", () => {
-                        window.print();
-                      });
-                    } else {
-                      messageHeader.innerHTML = `Something went wrong- it seems you tried to book multiple ticket try for the another show.`;
-                      message_displayObj.appendChild(messageHeader);
-                    }
-                  });
-              }
-            } else {
-              alert("please login first......");
-            }
-          });
-        });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    freeSeatsButton.addEventListener("click", () => {
+      availSeatsCard.style.display = "block";
+      const apiUrl3 = `http://3.90.205.148/bookings/freeseats/${date}/${hall}/${slot}`;
+      createSeatsArrayFromApi(16, apiUrl3);
+    });
   }
 
-  //------------------------------------------------------------------------------Getting userid from api using email-------------------------------------------------------
-  function get_userid_by_email(useremail) {
-    const api_for_id_toget_user = `http://3.90.205.148/users/${useremail}`;
-    fetch(api_for_id_toget_user)
-      .then((response) => response.json())
-      .then((userData) => {
-        user_id = userData.id;
-      });
-  }
-
-  //--------------------------------fetching poster_url from api---------------------------
-  function get_image_url_by_movie_name(movie_name, elementToattach) {
+  function renderMoviePoster(movie_name, elementToattach) {
     return fetch(
       `https://api.themoviedb.org/3/search/movie?api_key=eacfeabd8e111e3bea6edaa3358907aa&query=${movie_name}`
     )
       .then((response) => response.json())
       .then((movieObject) => {
-        const movieImgSrc = `https://image.tmdb.org/t/p/w500/${movieObject.results[0].poster_path}`;
+        const movieImgSrc = movieObject.results[0].poster_path
+          ? `https://image.tmdb.org/t/p/w500/${movieObject.results[0].poster_path}`
+          : "https://media.comicbook.com/files/img/default-movie.png";
         const moviePosterElement = document.createElement("img");
         moviePosterElement.setAttribute("src", movieImgSrc);
         moviePosterElement.style.minWidth = "200px";
@@ -267,3 +160,117 @@ export default () => {
       });
   }
 };
+
+/// -------------------------- free seeats display -----------------------------------------
+let freeseats_button_array = [];
+
+function createSeatsArrayFromApi(totalSeats, apistring) {
+  let freeseats = [];
+  for (let i = 1; i <= totalSeats; i++) {
+    freeseats.push(i);
+  }
+  const trElement = document.querySelector(".tr");
+  let counter = 0;
+  fetch(apistring)
+    .then((response) => response.json())
+    .then((seatsData) => {
+      seatsData.forEach((seat) => {
+        for (let i = 0; i < freeseats.length; i++) {
+          if (freeseats[i] === seat.seatNumber) {
+            freeseats.splice(i, 1);
+          }
+        }
+
+        if (counter % 4 === 0) {
+          const newTd = document.createElement("td");
+          const newTr = document.createElement("tr");
+          const buttonObj = document.createElement("button");
+          buttonObj.innerText = seat.seatNumber;
+          freeseats_button_array.push(buttonObj);
+          newTd.style.backgroundColor = "green";
+          newTd.appendChild(buttonObj);
+          trElement.appendChild(newTr);
+          trElement.appendChild(newTd);
+        } else {
+          const newTd = document.createElement("td");
+          const buttonObj = document.createElement("button");
+          buttonObj.innerText = seat.seatNumber;
+          freeseats_button_array.push(buttonObj);
+          newTd.style.backgroundColor = "green";
+          newTd.appendChild(buttonObj);
+          trElement.appendChild(newTd);
+        }
+
+        counter++;
+      });
+      for (let i = 0; i < freeseats.length; i++) {
+        const newTd = document.createElement("td");
+        const buttonObj = document.createElement("button");
+        buttonObj.innerText = freeseats[i];
+        buttonObj.disabled = "true";
+        newTd.style.backgroundColor = "red";
+        newTd.appendChild(buttonObj);
+        trElement.appendChild(newTd);
+      }
+      freeseats_button_array.forEach((btn) => {
+        btn.addEventListener("click", () => {
+          selected_seat = btn.innerText;
+
+          if (typeof localStorage.username !== "undefined") {
+            const connformation = confirm(
+              "Do you want to reserve seat number:- " + btn.innerText + "?"
+            );
+            if (connformation) {
+              const api4 = `http://3.90.205.148/bookings/${selectedDate}/${moviename}/${selectedHall}/${selectedSlot}/${user_id}/${btn.innerText}`;
+              fetch(api4)
+                .then((response) => response.json())
+                .then((reserved_seat) => {
+                  const message_displayObj = document.querySelector(
+                    ".conformation_message"
+                  );
+                  const messageHeader = document.createElement("h3");
+                  let objectLength = Object.keys(reserved_seat).length;
+                  if (objectLength > 0) {
+                    messageHeader.innerHTML = `You have reserved a seat number ${selected_seat},\n
+																		in hall no ${selectedHall} on ${selectedDate},${selectedSlot} show on\n
+																		to see the ${moviename} . Thank you for choosing us... have a nice show.`;
+
+                    message_displayObj.appendChild(messageHeader);
+                    btn.style.backgroundColor = "blue";
+                    btn.disabled = "true";
+                    trElement.style.display = "none";
+                    headerTag_element.innerHTML = "Your reservation datails.";
+                    const print_btn = document.createElement("button");
+                    print_btn.className = "print_btn";
+                    print_btn.innerText = "Print";
+                    message_displayObj.appendChild(print_btn);
+                    print_btn.addEventListener("click", () => {
+                      window.print();
+                    });
+                  } else {
+                    messageHeader.innerHTML = `Something went wrong- it seems you tried to book multiple ticket try for the another show.`;
+                    message_displayObj.appendChild(messageHeader);
+                  }
+                });
+            }
+          } else {
+            alert("please login first......");
+          }
+        });
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+//----------------------------- getting userid from api using email -------------------------------------
+function get_userid_by_email(useremail) {
+  const api_for_id_toget_user = `http://3.90.205.148/users/${useremail}`;
+  fetch(api_for_id_toget_user)
+    .then((response) => response.json())
+    .then((userData) => {
+      user_id = userData.id;
+    });
+}
+//------------------------------------------------------------------
