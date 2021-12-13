cbb13a19dfb7ab872e204951717985e990348ec9
krishnaamen <krishnaament@gmail.com>
2021-12-13 13:12:07 +0100

    fixed localhost ip.

--- utils.js
+++ utils.js
@@ -2,7 +2,7 @@ export default {
 	logout: (auth) => {
 		auth.signOut();
 		localStorage.clear();
-		window.location = "http://127.0.0.1:5501/";
+		window.location = "/";
 
 		alert("SignOut Successfully from System");
 	},
